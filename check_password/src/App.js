9dabc84bf1bf94426e2d4cb2bc907c11c8dcbdf1
MaryMisMaria <mariia.mys.mpmpm.2021@lpnu.ua>
2024-11-21 21:55:48 +0200

    create-check-password

---
+++ check_password/src/App.js
@@ -0,0 +1,16 @@
+import PasswordStrengthChecker from './components/check_password/check_password';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <title>Check Password</title>
+      </header>
+      <body>
+        <PasswordStrengthChecker />
+      </body>
+    </div>
+  );
+}
+
+export default App;
